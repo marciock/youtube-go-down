71d6086466e074748d24a93a467f1c44cc871d01
marciock <marciockalves@gmail.com>
2020-04-27 19:31:39 -0300

    youtube

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # include bible go get github.com/kkdai/youtube - compile from Linux into  fwindows - env GOOS=windows GOARCH=amd64 go build main.go
+
+Utilização
+
+
+
+Na linha de comando digite youtbe-down -n=nome do video -d=diretorio(opcional) http://youtube.com/urlparadownload.
+
+Os arquivos binários estão nas pastas respectivas aos seus Sistemas Operacionais.
--- main.go
+++ main.go
@@ -4,24 +4,32 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os/user"
+	//"os/user"
 	"path/filepath"
 
 	. "github.com/kkdai/youtube"
 )
 
 func main() {
+
+	customName :=flag.String("n","youtube","Name video")
+	customDir :=flag.String("d","./","Directory")
 	flag.Parse()
 	log.Println(flag.Args())
-	usr, _ := user.Current()
-	currentDir := fmt.Sprintf("%v/Vídeos/", usr.HomeDir)
-	log.Println("download to dir=", currentDir)
+	
+	//usr, _ := user.Current()
+	//currentDir := fmt.Sprintf("%v/Vídeos/", usr.HomeDir)
+	currentDir := fmt.Sprintf("%v/./", "./")
+	log.Println("download to dir=", *customDir)
 	y := NewYoutube(true)
 	arg := flag.Arg(0)
 	if err := y.DecodeURL(arg); err != nil {
 		fmt.Println("err:", err)
 	}
-	if err := y.StartDownload(filepath.Join(currentDir, "dl.mp4")); err != nil {
+	/*if err := y.StartDownload(filepath.Join(currentDir, "dl.mp4")); err != nil {
+		fmt.Println("err:", err)
+	}*/
+	if err := y.StartDownload(filepath.Join(currentDir, *customName+".mp4")); err != nil {
 		fmt.Println("err:", err)
 	}
 }
